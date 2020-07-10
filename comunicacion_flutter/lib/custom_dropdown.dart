81fe479035641d81d8f7d71ae1499f8e8663bbc1
Nicanor <rivero.diaz.nicanor123@gmail.com>
2020-07-10 18:46:34 -0300

    Clases F2 y F3 son statefullwidgets, F2 tiene 2 nuevos containers(Nombre y Hora) y F1 no tiene DateTime

--- comunicacion_flutter/lib/custom_dropdown.dart
+++ comunicacion_flutter/lib/custom_dropdown.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:date_field/date_field.dart';
 
-DateTime selectedData;
+//import 'package:date_field/date_field.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
+
+
+//DateTime selectedData;
 
 class F1 extends StatefulWidget {
   @override
@@ -43,7 +47,7 @@ class _F1State extends State<F1> {
                 ),
               ),
               SizedBox(height: 50),
-              Container(
+              /*Container(
                 width: 270,
                 child: DateField(
                   onDateSelected: (DateTime value) {
@@ -54,14 +58,15 @@ class _F1State extends State<F1> {
                   selectedDate: selectedData,
                 ),
               ),
+              */
               SizedBox(height: 50),
               Container(
                 width: 300,
                 child: TextField(
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
-                  decoration: InputDecoration(
-                      hintText: 'Justifique la falta de su hijo'),
+                  decoration:
+                      InputDecoration(hintText: 'Justifique de la falta'),
                 ),
               ),
               SizedBox(height: 35),
@@ -70,8 +75,71 @@ class _F1State extends State<F1> {
                 color: Colors.blue,
                 elevation: 6,
                 shape: StadiumBorder(),
-                onPressed: (){
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
+
+
+class F2 extends StatefulWidget {
+  @override
+  _F2State createState() => _F2State();
+}
+
+
+class _F2State extends State<F2> {
+  DateTime date3;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffe0e0e0),
+      body: Center(
+        child: Container(
+          width: 330,
+          height: 425,
+          decoration: BoxDecoration(
+            color: Color(0xffd6d6d6),
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 270,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  textCapitalization: TextCapitalization.words,
+                  decoration: InputDecoration(
+                    hintText: 'Nombre del Alumno',
+                    fillColor: Colors.grey,
+                    filled: true,
+                    contentPadding:
+                        new EdgeInsets.fromLTRB(5.0, 30.0, 10.0, 10.0),
+                    border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(12.0)),
+                  ),
+                ),
+              ),
+              DateTimePickerFormField(
+                inputType: InputType.time,
+                format: DateFormat("HH:mm"),
+                initialTime: TimeOfDay(hour: 5, minute: 5),
+                editable: false,
+                decoration: InputDecoration(
+                    labelText: 'Time', hasFloatingPlaceholder: false),
+                onChanged: (dt) {
+                  setState(() => date3 = dt);
+                  print('Selected date: $date3');
+                  print('Hour: $date3.hour');
+                  print('Minute: $date3.minute');
                 },
               ),
             ],
@@ -82,22 +150,31 @@ class _F1State extends State<F1> {
   }
 }
 
-class F2 extends StatelessWidget {
-  const F2({Key key}) : super(key: key);
 
+class F3 extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
-  }
+  _F3State createState() => _F3State();
 }
 
-class F3 extends StatelessWidget {
-  const F3({Key key}) : super(key: key);
-
+class _F3State extends State<F3> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Scaffold(
+      backgroundColor: Color(0xffe0e0e0),
+      body: Center(
+        child: Container(
+          width: 330,
+          height: 425,
+          decoration: BoxDecoration(
+            color: Color(0xffd6d6d6),
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Column(
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
   }
 }
