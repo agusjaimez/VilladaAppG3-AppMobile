829e0e89e303235210fa28b7882442eff064c420
Nicanor <rivero.diaz.nicanor123@gmail.com>
2020-07-30 15:07:46 -0300

    Agrego animacion de transicion al inkwell de Principal.dart, agrego archivo Slide.dart(sirve para la animacion). Modifico notificacionWidget.dart, agrego un boton que hace un pop al widget de Principal

--- comunicacion_flutter/lib/notificacionWidget.dart
+++ comunicacion_flutter/lib/notificacionWidget.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class Notificacion extends StatefulWidget {
+  @override
+  _NotificacionState createState() => _NotificacionState();
+}
+
+class _NotificacionState extends State<Notificacion> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: RaisedButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Text('Go back'),
+      )),
+    );
+  }
+}
