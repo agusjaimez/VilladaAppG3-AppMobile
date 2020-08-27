2825bcd50e3df0fb195d6616b92cf5cf2cf92cc3
jemf17 <40602289+jemf17@users.noreply.github.com>
2020-08-27 16:53:12 -0300

    agrege los formularios de confirmacion de las publicaciones de los eventos (los botones no tienen funcionalidad)

--- comunicacion_flutter/lib/screens/home/form_notifications.dart
+++ comunicacion_flutter/lib/screens/home/form_notifications.dart
@@ -3,6 +3,95 @@ import 'package:flutter/material.dart';
 class Form_notification extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+      ),
+      body: SingleChildScrollView(
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      'Titulo',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Sub-Titulo',
+                    style: TextStyle(
+                      color: Colors.teal,
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  '
+                      'palabras palabras palabras palabras palabras palabras palabras  ',
+                      softWrap: true,
+                    ),
+                  ),
+                  Container(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new RaisedButton(
+                          color: Colors.amber,
+                          textColor: Colors.black26,
+                          child: Text("Acepto"),
+                          onPressed: () {
+                            return true;
+                          }),
+                      new RaisedButton(
+                          color: Colors.amber,
+                          textColor: Colors.black26,
+                          child: Text("No Quiero"),
+                          onPressed: () {
+                            return false;
+                          })
+                    ],
+                  ))
+                ],
+              ),
+            ),
+            /*3*/
+          ],
+        ),
+      ),
+    );
   }
 }
